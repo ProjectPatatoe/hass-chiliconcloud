64f1fdda8ce27552ff9347008cd39e682d2048e2
ProjectPatatoe <projectpaatt@gmail.com>
2021-03-08 16:27:37 -0800

    blah, final commit before going to 'legacy'

--- __init__.py
+++ __init__.py
@@ -1,7 +1,32 @@
 """Chilicon Cloud integration."""
+from datetime import datetime as dt
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_USERNAME,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import aiohttp_client
+
+from .const import ( # pylint: disable=unused-import
+    CONF_INSTALLATION_HASH,
+    DOMAIN,
+    ROOT_URL,
+    LOGIN_URL,
+    INSTALLATION_URL,
+    UPDATE_URL,
+)
 
 #async def async_setup(hass,config):
 #        """hello world"""
 #        hass.states.async_set('chiliconcloud.current_production', 'unknown')
 #
 #        return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    if await hass.config_entries.async_forward_entry_unload(entry, "sensor"):
+        hass.data[DOMAIN].pop(entry.entry_id)
+        return True
+
+    return False
